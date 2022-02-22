fff5d9acd6c30cd34e3f88237f4d4610099e7697
Zeus <84690417+darkrut22@users.noreply.github.com>
2022-02-23 04:05:50 +0900

    updated button

--- public/script/animationText.js
+++ public/script/animationText.js
@@ -5,7 +5,7 @@ for (let i = 0; i < textWrappers.length; i++) {
   textWrappers[i].innerHTML =textWrappers[i].textContent.replace(/\S/g, "<span class='letter1'>$&</span>");  
 }
 
-anime.timeline({loop: true})
+anime.timeline({loop: 1})
   .add({
     targets: '.animation-text1 .letter1',
     translateY: [100,0],
@@ -14,14 +14,15 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     duration: 1400,
     delay: (el, i) => 0 + 20 * i
-  }).add({
-    targets: '.animation-text1 .letter1',
-    translateY: [0,-100],
-    opacity: [1,0],
-    easing: "easeInExpo",
-    duration: 1200,
-    delay: (el, i) => 100 + 20 * i
-  });
+  })
+  // .add({
+  //   targets: '.animation-text1 .letter1',
+  //   translateY: [0,-100],
+  //   opacity: [1,0],
+  //   easing: "easeInExpo",
+  //   duration: 1200,
+  //   delay: (el, i) => 100 + 20 * i
+  // });
 
   
 // Wrap every letter in a span
@@ -31,7 +32,7 @@ for (let i = 0; i < textWrappers2.length; i++) {
   textWrappers2[i].innerHTML =textWrappers2[i].textContent.replace(/\S/g, "<span class='letter2'>$&</span>");  
 }
 
-anime.timeline({loop: true})
+anime.timeline({loop: 1})
   .add({
     targets: '.animation-text2 .letter2',
     translateY: [100,0],
@@ -40,14 +41,15 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     duration: 1200,
     delay: (el, i) => 300 + 10 * i
-  }).add({
-    targets: '.animation-text2 .letter2',
-    translateY: [0,-100],
-    opacity: [1,0],
-    easing: "easeInExpo",
-    duration: 200,
-    delay: (el, i) => 100 + 20 * i
-  });
+  })
+  // .add({
+  //   targets: '.animation-text2 .letter2',
+  //   translateY: [0,-100],
+  //   opacity: [1,0],
+  //   easing: "easeInExpo",
+  //   duration: 200,
+  //   delay: (el, i) => 100 + 20 * i
+  // });
   
   // Wrap every letter in a span
   var textWrappers2 = document.querySelectorAll('.animation-text3');
@@ -56,7 +58,7 @@ anime.timeline({loop: true})
     textWrappers2[i].innerHTML =textWrappers2[i].textContent.replace(/\S/g, "<span class='letter3'>$&</span>");  
   }
   
-  anime.timeline({loop: true})
+  anime.timeline({loop: 1})
     .add({
       targets: '.animation-text3 .letter3',
       translateY: [100,0],
@@ -65,11 +67,12 @@ anime.timeline({loop: true})
       easing: "easeOutExpo",
       duration: 2000,
       delay: (el, i) => 0 + 40 * i
-    }).add({
-      targets: '.animation-text3 .letter3',
-      translateY: [0,-100],
-      opacity: [1,0],
-      easing: "easeInExpo",
-      duration: 1200,
-      delay: (el, i) => 100 + 40 * i
-    });
+    })
+    // .add({
+    //   targets: '.animation-text3 .letter3',
+    //   translateY: [0,-100],
+    //   opacity: [1,0],
+    //   easing: "easeInExpo",
+    //   duration: 1200,
+    //   delay: (el, i) => 100 + 40 * i
+    // });
